a3ca1737818302324615a40f307bb4fc63e98c90
gavillar <gabriel_gomesvillar@hotmail.com>
2023-04-15 01:10:23 -0300

    Create Class DTO for Tecnico.

--- helpdesk/src/main/java/br/com/helpdesk/resources/TecnicoResource.java
+++ helpdesk/src/main/java/br/com/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package br.com.helpdesk.resources;
 
 import br.com.helpdesk.domain.Tecnico;
+import br.com.helpdesk.domain.dtos.TecnicoDTO;
 import br.com.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,9 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
 }
