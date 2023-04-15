65446855bf8c2f4ee2f928028bd358c99c1a5a45
gavillar <gabriel_gomesvillar@hotmail.com>
2023-04-15 02:26:24 -0300

    Exception for object not Found.

--- helpdesk/src/main/java/br/com/helpdesk/services/TecnicoService.java
+++ helpdesk/src/main/java/br/com/helpdesk/services/TecnicoService.java
@@ -3,6 +3,7 @@ package br.com.helpdesk.services;
 
 import br.com.helpdesk.domain.Tecnico;
 import br.com.helpdesk.repositories.TenicoRepository;
+import br.com.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
@@ -15,7 +16,7 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado! " + id));
     }
 
 }
---
+++ helpdesk/src/main/java/br/com/helpdesk/services/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.com.helpdesk.services.exceptions;
+
+
+import br.com.helpdesk.domain.enums.Status;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError>
+    objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+
+        StandardError error = new StandardError(System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(), "Object Not Found",
+                ex.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+}
+
+
+
+
