672e4b284fde808de2dd7676093d44c68a91679e
gavillar <gabriel_gomesvillar@hotmail.com>
2023-04-29 21:14:31 -0300

    remove changes docker

--- helpdesk/src/main/java/br/com/helpdesk/HelpdeskApplication.java
+++ helpdesk/src/main/java/br/com/helpdesk/HelpdeskApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelpdeskApplication {
 
-	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
